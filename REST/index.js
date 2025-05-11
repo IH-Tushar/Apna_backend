b1c7b3483f92820f9a199c08a3dc6db87af1f431
IH-TUSHAR <mohammodtushar.cb@gmail.com>
2025-05-11 18:12:29 +0600

    rest

--- REST/index.js
+++ REST/index.js
@@ -4,8 +4,10 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const { v4: uuidv4 } =  require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
@@ -57,9 +59,21 @@ app.patch("/posts/:id", (req, res) => {
     let newContent = req.body.content;
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
-    console.log(id);
-    res.send("patch req is waiting");
+    console.log(post);
+    res.redirect("/posts")
+});
+
+app.get("/posts/:id/edit", (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit.ejs", { post });
 })
+
+app.delete("/posts/:id", (req,res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts")
+});
 
 app.listen(port, () => {
     console.log("server is runing in port : 8080");
